c315883f8cf65840b5c38dcbd29bbff62ac6ae9f
alexsh882 <alexsh882@gmail.com>
2024-11-13 14:55:11 -0300

    fix: Wrap logo in a link to navigate to the homepage

--- frontend/src/features/ui/logo.tsx
+++ frontend/src/features/ui/logo.tsx
@@ -1,10 +1,11 @@
 import { Bot } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export function Logo() {
   return (
-    <>
+    <Link to={"/"} className="flex justify-center items-center gap-2">
       <Bot className="size-8" />
       <span className="text-2xl font-sans-accent">DocChat</span>
-    </>
+    </Link>
   );
 }
