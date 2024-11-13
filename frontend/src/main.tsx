a34ef6fe493043b55d97d56f078e2cba58caea81
alexsh882 <alexsh882@gmail.com>
2024-11-13 16:08:15 -0300

    fix: Integrate QueryClientProvider for React Query in main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { ChatLayout } from "./layouts/chat";
 import MainLayout from "./layouts/main";
 import ChatBot from "./pages/chatbot";
 import Home from "./pages/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/chat",
+    path: "/chat/:id?",
     element: <ChatLayout />,
     children: [
       {
@@ -30,8 +31,13 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
