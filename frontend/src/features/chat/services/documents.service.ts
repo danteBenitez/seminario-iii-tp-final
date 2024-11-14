397715fb08fd912ca3e9395cbfd36fd56d943420
danteBenitez <benitezdante123@gmail.com>
2024-11-14 13:19:35 -0300

    feat: Add delete document service

--- frontend/src/features/chat/services/documents.service.ts
+++ frontend/src/features/chat/services/documents.service.ts
@@ -1,3 +1,5 @@
+import { resolveUrl } from "@/utils/resolve-url";
+
 type TDocument = {
   id: number;
   original_filename: string;
@@ -7,7 +9,7 @@ type TDocument = {
 export async function getUserDocuments(user_id: string | null) {
   try {
     const response = await fetch(
-      `http://localhost:8000/api/users/${user_id}/documents`
+      resolveUrl(`/api/users/${user_id}/documents`)
     );
 
     if (!response.ok) {
@@ -27,7 +29,7 @@ export async function getUserDocuments(user_id: string | null) {
 export async function getUserDocument(user_id: string, document_id: string) {
   try {
     const response = await fetch(
-      `http://localhost:8000/api/users/${user_id}/documents/${document_id}`
+      resolveUrl(`/api/users/${user_id}/documents/${document_id}`)
     );
 
     if (!response.ok) {
@@ -37,6 +39,29 @@ export async function getUserDocument(user_id: string, document_id: string) {
     const data = await response.json();
 
     console.log("getUserDocument", data);
+
+    return data as TDocument;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteUserDocument(user_id: string, document_id: string) {
+  try {
+    const response = await fetch(
+      resolveUrl(`/api/users/${user_id}/documents/${document_id}`),
+      {
+        method: "DELETE"
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Error al borrar documento.");
+    }
+
+    const data = await response.json();
+
+    console.log("deleteDocument", data);
 
     return data as TDocument;
   } catch (error) {
