4750af25c8cee46f2e2f2f1374fe32aa36b18ed2
alexsh882 DPC <alexsh882@gmail.com>
2025-03-01 13:52:05 -0300

    fix: corregir mensaje de impresión para EMBED_MODEL_NAME en __init__.py

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -11,7 +11,7 @@ LLM_MODEL_NAME= os.getenv("LLM_MODEL_NAME", "llama3.2:1b")
 print(f"LLM_MODEL_NAME = {LLM_MODEL_NAME}")
 
 EMBED_MODEL_NAME = os.getenv("EMBED_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2")
-print(f"LLM_MODEL_NAME = {EMBED_MODEL_NAME}")
+print(f"EMBED_MODEL_NAME = {EMBED_MODEL_NAME}")
 
 CHROMA_DIR = os.getenv("CHROMA_DIR", "chroma_db")
 print(f"CHROMA_DIR = {CHROMA_DIR}")
